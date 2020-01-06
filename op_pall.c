e33968377de14bb241a9cff841c924a5f298ddf5
steven-cruz <stevencruz.300008@gmail.com>
2020-01-06 03:50:18 +0000

    upgrade

--- op_pall.c
+++ op_pall.c
@@ -1,10 +1,10 @@
 #include "monty.h"
 
 /**
- * op_pall -function to print all elements in a stack.
- * @stack: double pointer of type sactk_t to head of doubly linked list.
- * @line_number: unsigned int for line number.
- * Return: no return, print list.
+ * op_pall - function to print all elements in a stack
+ * @stack: double pointer of type stack_t to head of doubly linked list
+ * @line_number: unsigned int for line number(void in this function)
+ * Return: no return, print list
  */
 void op_pall(stack_t **stack, __attribute__((unused))unsigned int line_number)
 {
